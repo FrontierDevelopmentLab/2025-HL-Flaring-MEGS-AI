d757294c129daa39b20b23bee7d430ed6d9992c4
Schirni <christoph.schirninger@edu.uni-graz.at>
2024-07-30 16:50:54 +0000

    implement DEM base model

--- irradiance/models/base_model.py
+++ irradiance/models/base_model.py
@@ -59,7 +59,6 @@ class BaseModel(LightningModule):
         self.log("valid_loss", loss, on_epoch=True, prog_bar=True, logger=True)
         self.log("valid_MAE", mae, on_epoch=True, prog_bar=True, logger=True)
         self.log("valid_RAE", av_rae, on_epoch=True, prog_bar=True, logger=True)
-        # [self.log(f"valid_RAE_{i}", err, on_epoch=True, prog_bar=True, logger=True) for i, err in enumerate(av_rae_wl)]
         self.log("valid_correlation_coefficient", cc, on_epoch=True, prog_bar=True, logger=True)
 
         return loss
@@ -85,7 +84,6 @@ class BaseModel(LightningModule):
         self.log("test_loss", loss, on_epoch=True, prog_bar=True, logger=True)
         self.log("test_MAE", mae, on_epoch=True, prog_bar=True, logger=True)
         self.log("test_RAE", av_rae, on_epoch=True, prog_bar=True, logger=True)
-        #[self.log(f"valid_RAE_{i}", err, on_epoch=True, prog_bar=True, logger=True) for i, err in enumerate(av_rae_wl)]
         self.log("test_correlation_coefficient", cc, on_epoch=True, prog_bar=True, logger=True)
 
         return loss
@@ -99,3 +97,83 @@ class BaseModel(LightningModule):
         norm_stdev = eve_norm[1]
         y = y * norm_stdev[None].to(y) + norm_mean[None].to(y)
         return y
+    
+
+
+class BaseDEMModel(LightningModule):
+
+    def __init__(self, eve_norm, uv_norm, model, t_query_points, loss_func=HuberLoss(), lr=1e-4):
+        super().__init__()
+        self.eve_norm = eve_norm
+        self.uv_norm = uv_norm
+        self.loss_func = loss_func
+        self.model = model
+        self.lr = lr
+        self.t_query_points = t_query_points
+
+    def forward(self, x):
+        raise NotImplementedError("Forward method not implemented, please implement it in the child class.")
+
+    def forward_unnormalize(self, x):
+        x = self.forward(x)
+        return self.unnormalize(x, self.eve_norm)
+        
+    def training_step(self, batch, batch_nb):
+        x, y = batch
+        x = x.unfold(2, 3, 1).unfold(3, 3, 1).reshape(x.shape[0], x.shape[1], -1, 3, 3) # channel, pixels, 3x3
+        y = x[:, :, :, 1, 1] # central pixel
+        y_pred = self(x)
+        loss = self.loss_func(y_pred, y)
+
+        epsilon = sys.float_info.min
+        rae = torch.abs((y - y_pred) / (torch.abs(y) + epsilon)) * 100
+        self.log("train_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_RAE", rae.mean(), on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_nb):
+        x, y = batch
+        x = x.unfold(2, 3, 1).unfold(3, 3, 1).reshape(x.shape[0], x.shape[1], -1, 3, 3) # channel, pixels, 3x3
+        y = x[:, :, :, 1, 1] # central pixel
+        y_pred = self(x)
+        loss = self.loss_func(y_pred, y)
+
+        #computing relative absolute error
+        epsilon = sys.float_info.min
+        rae = torch.abs((y - y_pred) / (torch.abs(y) + epsilon)) * 100
+        av_rae = rae.mean()
+        av_rae_wl = rae.mean(0)
+        # compute average cross-correlation
+        cc = torch.tensor([torch.corrcoef(torch.stack([y[i], y_pred[i]]))[0, 1] for i in range(y.shape[0])]).mean()
+        # mean absolute error
+        mae = torch.abs(y - y_pred).mean()
+
+        self.log("valid_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("valid_MAE", mae, on_epoch=True, prog_bar=True, logger=True)
+        self.log("valid_RAE", av_rae, on_epoch=True, prog_bar=True, logger=True)
+        self.log("valid_correlation_coefficient", cc, on_epoch=True, prog_bar=True, logger=True)
+
+        return loss
+
+    def test_step(self, batch, batch_nb):
+        x, y = batch
+        x = x.unfold(2, 3, 1).unfold(3, 3, 1).reshape(x.shape[0], x.shape[1], -1, 3, 3) # channel, pixels, 3x3
+        y = x[:, :, :, 1, 1] # central pixel
+        y_pred = self(x)
+        loss = self.loss_func(y_pred, y)
+
+        #computing relative absolute error
+        epsilon = sys.float_info.min
+        rae = torch.abs((y - y_pred) / (torch.abs(y) + epsilon)) * 100
+        av_rae = rae.mean()
+        # compute average cross-correlation
+        cc = torch.tensor([torch.corrcoef(torch.stack([y[i], y_pred[i]]))[0, 1] for i in range(y.shape[0])]).mean()
+        # mean absolute error
+        mae = torch.abs(y - y_pred).mean()
+
+        self.log("test_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("test_MAE", mae, on_epoch=True, prog_bar=True, logger=True)
+        self.log("test_RAE", av_rae, on_epoch=True, prog_bar=True, logger=True)
+        self.log("test_correlation_coefficient", cc, on_epoch=True, prog_bar=True, logger=True)
+
+        return loss
